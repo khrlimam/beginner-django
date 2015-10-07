75599f553194a217c36b5696ae7984a1a76426ab
khyrulimam <ki65559@gmail.com>
2015-10-07 14:41:51 +0800

    editing view

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.template import loader, RequestContext
 from .models import Questions
+from django import middleware
 # Create your views here.
 
 def index(request):
     latest_question_list = Questions.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {'latest_question_list': latest_question_list})
-    return HttpResponse(template.render(context))
+    template = 'polls/index.html'
+    context = {'latest_question_list': latest_question_list}
+    return render(request, template, context)
 
 def detail(request, question_id):
-    return HttpResponse('You\'re looking question at %s' % question_id)
+    question = get_object_or_404(Questions, pk=question_id)
+    return render(request, "polls/detail.html", {'question':question})
     
 def results(request, question_id):
     response = 'Your\'re looking at results of the question id %s.'
