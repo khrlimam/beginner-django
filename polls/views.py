0efe6abcaee33710b3a68209af4632a120b3ecf2
Khairul Imam <ki65559@gmail.com>
2015-10-10 15:49:50 +0800

    mantap

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
 from .models import Questions
-from django import middleware
+
 
 # Create your views here.
 
