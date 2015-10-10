f6bb57f3aa5fbea53501a2d50370ac12aaa4165e
Khairul Imam <ki65559@gmail.com>
2015-10-10 17:58:55 +0800

    adding result.html file, handling save votes request

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
+from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
-from .models import Questions
+from .models import Questions, Choices
 
 
 # Create your views here.
@@ -19,9 +20,20 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = 'Your\'re looking at results of the question id %s.'
-    return HttpResponse(response % question_id)
+    q = Questions.objects.get(pk=question_id)
+    return render(request, 'polls/result.html', {
+        'success_message':'Successfully adding vote to choice of question %s' % q.question_text})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're vooting on question id %s" % question_id)
+    p = get_object_or_404(Questions, pk=question_id)
+    try:
+        selected_choice = p.choices_set.get(pk=request.POST['choice'])
+    except(KeyError, Choices.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'error_message': 'You have to choice something!',
+            'question':p})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:result', args=(p.id,)))
